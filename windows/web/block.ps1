0f1d9fea449b23edb5efe6437f8db341aafccc42
BrightSpecter <neuralnexuslab@hotmail.com>
2026-07-23 09:45:38 +0800

    Update block.ps1

--- windows/web/block.ps1
+++ windows/web/block.ps1
@@ -1,4 +1,4 @@
-$group = "NXLab-WebBlock"
+$group = "webblock"
 Get-NetFirewallRule -Group $group -ErrorAction SilentlyContinue |
     Remove-NetFirewallRule
 New-NetFirewallRule -DisplayName "Block HTTP TCP" -Group $group -Direction Outbound -Protocol TCP -RemotePort 80 -Action Block
